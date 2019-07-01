83e0bcf97286ccd1947330159b1edf2e859b5ff8
komangmahendra <komangwp.mahendra@gmail.com>
2019-07-01 15:28:00 +0700

    add description

--- cms/client/sulap-frontend/src/components/Homepage.js
+++ cms/client/sulap-frontend/src/components/Homepage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Container } from 'react-bootstrap';
 
 const styles = {
     containerHomePage: {
@@ -9,18 +9,19 @@ const styles = {
         fontSize:'20vh', letterSpacing:'30px', fontWeight:'200'
     },
     homePageSulapInfo: {
-        letterSpacing:'2px', fontSize:'30px', color:'grey', fontWeight:'200'
+        letterSpacing:'2px', fontSize:'20px', color:'grey', fontWeight:'200', textAlign:'center'
     }
 }
 
 export default function Homepage() {
     return (
         <div style={styles.containerHomePage}>
-            <p style={styles.homePageSulap}> <b>SULAP</b><span style={{ color:'#00b894'}}>-JS</span></p>
-            <p style={styles.homePageSulapInfo}> 
-                package for create <b> cms </b><br/>
-                npm install -g <b>sulap</b>
-            </p> 
+            <Container >
+                <p style={styles.homePageSulap}> <b>SULAP</b><span style={{ color:'#00b894'}}>-JS</span></p>
+                <p style={styles.homePageSulapInfo}> 
+                    Sulap JS is a JavaScript Content Management System Generator which provides users an instant client-server website model. It includes user management system and simple CRUD model. The package is mostly powered by Node Js and MongoDb. In client-side, we use React Js as its framework and in server-side, we use express Js for its REST. It aims to assist users or developers for saving time in initiating  their website. By using 2 commands, a user is able to build a simple client-server website. The package also provides users to put some options or configuration while initiating their website, so it eases users in developing their website.
+                </p> 
+            </Container>
         </div>
     )
 }
