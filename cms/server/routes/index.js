f200c5bd78a432d8af3636179f1b6c834842a102
Michael <msaputra93@yahoo.com>
2019-07-02 21:42:29 +0700

    add multer server, type image -> string mongoose

--- cms/server/routes/index.js
+++ cms/server/routes/index.js
@@ -1,12 +1,24 @@
 const router = require('express').Router()
 const userRouter = require('./user')
 const AuthController = require('../controllers/auth')
+const UploadController = require('../controllers/upload')
+const multer  = require('multer')
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + Date.now()+ '.jpeg')
+    }
+})
+const upload = multer({ storage })
 //sulap-route-source
 //add 
 //`const ${modelname}Router = require('./${modelname})`
 
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
+router.post('/upload', upload.single('file'), UploadController.uploadImage)
 router.use('/user', userRouter)
 //sulap-add-route
 /* please do not delete comment above  */
