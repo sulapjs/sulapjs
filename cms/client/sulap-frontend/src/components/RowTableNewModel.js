98612db1d34b4765260b7a5cebe8d2f75ea3f675
komangmahendra <komangwp.mahendra@gmail.com>
2019-07-03 01:11:19 +0700

    add image, text area

--- cms/client/sulap-frontend/src/components/RowTableNewModel.js
+++ cms/client/sulap-frontend/src/components/RowTableNewModel.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button } from 'react-bootstrap';
+import { Button, Image } from 'react-bootstrap';
 import moment from 'moment';
 
 export default function RowTableNewModel(props) {
@@ -15,9 +15,11 @@ export default function RowTableNewModel(props) {
         <tr>
             <td>{ index+1 }</td>
             { Object.keys(value).map( (el, elIndex) => {
-                return ( el ==='updated' || el ==='created' || el ==='_id' || key_model.includes(el) ? <td key={ elIndex }>{ 
-                    el ==='created' || el === 'updated' ?  moment(value[el]).format('MMMM Do YYYY, h:mm:ss a') : value[el]  
-                }</td> : null  )
+                return ( el ==='updated' || el ==='created' || el ==='_id' || key_model.includes(el) ? 
+                    <td  align='center' key={ elIndex }>{ el ==='created' || el === 'updated' ?  moment(value[el]).format('MMMM Do YYYY, h:mm:ss a') : (value[el].split('.')[1]  === 'jpeg' || 
+                    value[el].split('.')[1]  === 'jpg' || value[el].split('.')[1]  === 'png' ? <Image width="50px" height='50px'src={ `http://localhost:3000/uploads/${value[el]}`} roundedCircle /> : value[el]
+                    )  
+                } </td> : null  )
             })}
             <td colSpan={2}> <Button size='sm' onClick={(e) => props.edit(value)}><i className="fas fa-edit"></i></Button> <Button size='sm' onClick={(e) => props.delete(value._id)}><i className="fas fa-trash"></i></Button> </td>
         </tr>  
