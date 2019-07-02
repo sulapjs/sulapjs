605b00530ddd5ab752b402f8c6256cf0f9fd947a
komangmahendra <komangwp.mahendra@gmail.com>
2019-07-02 15:46:23 +0700

    integrate Search, Edit , Delete done

--- cms/client/sulap-frontend/src/components/RowTableNewModel.js
+++ cms/client/sulap-frontend/src/components/RowTableNewModel.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Button } from 'react-bootstrap';
+import moment from 'moment';
 
 export default function RowTableNewModel(props) {
 
@@ -13,8 +15,11 @@ export default function RowTableNewModel(props) {
         <tr>
             <td>{ index+1 }</td>
             { Object.keys(value).map( (el, elIndex) => {
-                return ( el == 'updated' || el == 'created' || el =='_id' || key_model.includes(el) ? <td key={ elIndex }>{ value[el] }</td> : null  )
+                return ( el ==='updated' || el ==='created' || el ==='_id' || key_model.includes(el) ? <td key={ elIndex }>{ 
+                    el ==='created' || el === 'updated' ?  moment(value[el]).format('MMMM Do YYYY, h:mm:ss a') : value[el]  
+                }</td> : null  )
             })}
+            <td colSpan={2}> <Button size='sm' onClick={(e) => props.edit(value)}><i className="fas fa-edit"></i></Button> <Button size='sm' onClick={(e) => props.delete(value._id)}><i className="fas fa-trash"></i></Button> </td>
         </tr>  
         </>
     )
