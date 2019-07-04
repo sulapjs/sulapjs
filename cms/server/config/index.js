852aa84953afbc37789d8ded5e46b6015d7df781
Michael <msaputra93@yahoo.com>
2019-07-04 14:08:48 +0700

    update docs, edit comment mongo url

--- cms/server/config/index.js
+++ cms/server/config/index.js
@@ -1,9 +1,13 @@
 //this file is for dynamic file naming
 
-module.exports ={
+module.exports = {
     mongoUrl: process.env.MONGO_URL || (
+        //insert your database url here
         /*databaseName*/ 
         /** please do not delete comment above */ 
+        //if test is run --> url below will be used,
+        //else /*databaseName*/ will be replaced with user's input
+        //database below is for testing
         'mongodb://localhost:27017/sulap-example_'+ process.env.NODE_ENV
         )
 }
